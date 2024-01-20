08176420850bf6e67b3c901648c7f4f1c9c0299d
sudo-CodeBoyBebop <42tjhenry@gmail.com>
2024-01-20 14:10:09 -0800

    Implement Sprite class

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
+
+// set canvas background to black
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+
+class Sprite {
+    constructor(position) {
+        this.position = position;
+    }
+};
+
+const player = new Sprite({
+    x: 0,
+    y: 0
+});
+
+console.log(player);
